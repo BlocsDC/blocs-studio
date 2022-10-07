fba9f6cd798385684ecb26463b6721ccd8e9846e
BlocsDC <114787561+BlocsDC@users.noreply.github.com>
2022-10-07 13:01:18 -0600

    file flips
    changing files

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
-// Call and config dotenv
-import dotenv from 'dotenv';
-dotenv.config();
-// Express Imports
-import express from 'express';
 
 // Import Routes Here
-import { homeRoute } from './servers/server.js';
-
-// Initialise express App
-const App = express();
-
-// Set Process Evn for Dotenv
-const PORT = process.env.PORT || 5050;
-
-// Connect MongoDB with Moongose 
-
-// Set Express Apps
-App.set('view engine', 'ejs');
-
-// Use Express Apps
-App.use(express.static('public'));
-App.use(express.json())
-
-// Initialise Routes/Pages
-App.use('/', homeRoute);
+import App from './servers/server.js';
 
 
-// Server Listen and can wrap server with Database in the future...
-App.listen(PORT, (err) => {
-    if (err) throw err;
-    else
-    console.log(`Server runing on port: http://localhost:${PORT}`);
-});
 
-// Set Error Handlers Here if more.
-App.use((req, res) => {
-    res.status(404).render('404', {title: '404'})
-});
+const StartServer = () => App;
